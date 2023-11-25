8d8e1a0949bfbcb050727d074231f2cfb3dd15ea
Asim Thaha <iamasimthaha@gmail.com>
2023-11-25 22:26:53 +0530

    created the base models-user registration

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class UserRegistration(models.Model):
+    """Model definition for UserRegistration."""
+
+    # TODO: Define fields here
+    userid = models.AutoField(primary_key=True,)
+    name=models.CharField(default="",max_length=100)
+    email=models.EmailField(default="",max_length=254)
+    password=models.CharField(default="",max_length=100)
+    phone=models.IntegerField(null=True)
+    address=models.CharField(default="",max_length=400)
+    
+
+
+
+    class Meta:
+        """Meta definition for UserRegistration."""
+
+        verbose_name = 'UserRegistration'
+        verbose_name_plural = 'UserRegistrations'
+
+    def __str__(self):
+        """Unicode representation of UserRegistration."""
+        pass
