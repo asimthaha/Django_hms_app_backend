705a057e6040db0728b8ed4329923e5f331b14f8
Asim Thaha <iamasimthaha@gmail.com>
2023-11-29 14:06:29 +0530

    made doctor registration models

--- staff_app/models.py
+++ staff_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class DoctorRegistrationModel(models.Model):
+    
+    SPECIALITY = (
+        ("Dentist", "Dentist"),
+        ("Gynecologist", "Gynecologist"),
+        ("Genaral Physician", "Genaral Physician"),
+        ("Dermatologist", "Dermatologist"),
+        ("ENT specialist", "ENT specialist"),
+        ("Nephrologist", "Nephrologist"),
+        ("Cardiologist", "Cardiologist"),
+        ("Oncologist", "Oncologist"),
+    )
+
+    doctorid= models.AutoField(primary_key=True)
+    name=models.CharField(default="",max_length=100)
+    username=models.CharField(default="", max_length=30)
+    speciality=models.CharField(default="",
+        max_length=100,
+        blank=True, 
+        null=True,
+        choices=SPECIALITY,
+        help_text="Speciality")
+    startYear=models.IntegerField(null=True)
+    password=models.CharField(max_length=50, default="")
+    qualification=models.CharField(default="",max_length=100)
+    role=models.CharField(default="", max_length=100)
+
+    class Meta:
+        verbose_name = ("DoctorRegistrationModel")
+        verbose_name_plural = ("DoctorRegistrationModels")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("DoctorRegistrationModel_detail", kwargs={"pk": self.pk})
--- staff_app/views.py
+++ staff_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from staff_app.models import *
+from staff_app.serializer import *
+from django.db.models import Q
 
 # Create your views here.
+@csrf_exempt
+def LoginView(request):
+    if request.method=="POST":
+        data = json.loads(request.body)
+        getUsername = data['username']
+        getPassword = data['password']
+        loginData = DoctorRegistrationModel.objects.filter(Q(username__exact=getUsername) & Q(password__exact = getPassword)).values()
+        loginData = list(loginData)
+        return HttpResponse(json.dumps(loginData))
+    
+@csrf_exempt
+def displayDoctorView(request):
+    if request.method =="POST":
+        data= json.loads(request.body)
+        getId = data['doctorid']
+        data = DoctorRegistrationModel.objects.filter(Q(doctorid__exact=getId)).values()
+        data = list(data)
+        return HttpResponse(json.dumps(data))
